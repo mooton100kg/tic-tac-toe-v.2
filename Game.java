a2f6840b1f9abe8aad482e844e9d456dbac5a17a
mooton <mooton100kg@gmail.com>
2022-06-13 00:05:26 +0700

    'onefile'

--- Game.java
+++ Game.java
@@ -1,7 +1,3 @@
-package main;
-
-import static main.Board.*;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -94,6 +90,11 @@ public class Game{
                 return;
             }
         }
+
+        if (player1Po.size() + player2Po.size() == 7){
+            playing = false;
+            winner = "tie";
+        }
     }
 
     private int[][] getWinCase() {
@@ -108,5 +109,77 @@ public class Game{
             {3,6,9}
         };
         return winCase;
+    }
+    private String[][] getEmptyBoard(){
+        String[][] emptyBoard = {
+            {" ", "|", " ", "|", " ", "     |     1|2|3"},
+            {"-", "+", "-", "+", "-", "     |     -+-+-"},
+            {" ", "|", " ", "|", " ", "     |     4|5|6"},
+            {"-", "+", "-", "+", "-", "     |     -+-+-"},
+            {" ", "|", " ", "|", " ", "     |     7|8|9"},
+        };
+
+        return emptyBoard;
+    }
+
+    private int[] convertPo(int po){
+        int x = 0,y = 0;
+
+        if (po == 1){
+            y = 0;
+            x = 0;
+        }
+        else if (po == 2){
+            y = 0;
+            x = 2;
+        }
+        else if (po == 3){
+            y = 0;
+            x = 4;
+        }
+        else if (po == 4){
+            y = 2;
+            x = 0;
+        }
+        else if (po == 5){
+            y = 2;
+            x = 2;
+        }
+        else if (po == 6){
+            y = 2;
+            x = 4;
+        }
+        else if (po == 7){
+            y = 4;
+            x = 0;
+        }
+        else if (po == 8){
+            y = 4;
+            x = 2;
+        }
+        else if (po == 9){
+            y = 4;
+            x = 4;
+        }
+        int[] index = new int[]{y,x};
+        return index;
+    }
+
+    private String[][] setBorad(int newPo, String player, ArrayList<Integer> oldPo1, ArrayList<Integer> oldPo2){
+        String[][] board = getEmptyBoard();
+
+        int[] index = convertPo(newPo);
+        board[index[0]][index[1]] = player;
+
+        for (int i: oldPo1){
+            index = convertPo(i);
+            board[index[0]][index[1]] = "O";
+        }
+
+        for (int i: oldPo2){
+            index = convertPo(i);
+            board[index[0]][index[1]] = "X";
+        }
+        return board;
     }
 }
